db3d4ba463ed9113b96d6a31d7ef59509fb75317
paulomatheus <paulomatheus.araujo@hotmail.com>
2025-09-24 16:53:08 -0300

    Add Jump in the PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,11 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb2d; 
     public float speed = 2f;
+    public float jumpForce = 5f;
     Vector2 input;
+    bool onGround;
+    bool onWall;
+
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
@@ -18,5 +22,25 @@ public class PlayerController : MonoBehaviour
     public void OnMove(InputValue value)
     {
         input = value.Get<Vector2>();
+    }
+
+    public void OnJump(InputValue value)
+    {
+        if (!onGround && !onWall) return;
+
+        if (onWall) onWall = false;
+
+        Debug.Log("Jumped");
+        rb2d.linearVelocityY = jumpForce;
+    }
+
+    void SetFaceDirection(int faceDirection)
+    {
+        int faceFactor = faceDirection == transform.localScale.x ? 1 : -1;
+
+        transform.localScale = new Vector3(
+            transform.localScale.x * faceFactor,
+            transform.localScale.y,
+            transform.localScale.z);
     }
 }
