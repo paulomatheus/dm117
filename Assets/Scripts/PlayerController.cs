3fb8900fdc8a5d8aa24bb56765d8861707c7bf9d
Marcos <mcs.corazza@gmail.com>
2025-09-25 08:56:09 -0300

    Updated Mode and Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,15 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb2d; 
-    public float speed = 2f;
-    public float jumpForce = 5f;
     Vector2 input;
+    public float speed = 2f;
+    public float jumpForce = 2f;
     bool onGround;
-    bool onWall;
+    bool onRamp;
 
     void Start()
     {
@@ -22,22 +23,49 @@ public class PlayerController : MonoBehaviour
     public void OnMove(InputValue value)
     {
         input = value.Get<Vector2>();
+        if (input.x == 0.0) return;
+        
+        int faceDirection = input.x > 0 ? 1 : -1;
+        SetFaceDirection(faceDirection);
+        Debug.Log("Moving: " + input);
     }
 
     public void OnJump(InputValue value)
     {
-        if (!onGround && !onWall) return;
-
-        if (onWall) onWall = false;
+        if (!onGround && !onRamp) return;
 
         Debug.Log("Jumped");
         rb2d.linearVelocityY = jumpForce;
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            onGround = true;
+        }
+        if (collision.gameObject.CompareTag("Ramps"))
+        {
+            onRamp = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            onGround = false;
+        }
+        if (collision.gameObject.CompareTag("Ramps"))
+        {
+            onRamp = false;
+        }
+    }
+
     void SetFaceDirection(int faceDirection)
     {
-        int faceFactor = faceDirection == transform.localScale.x ? 1 : -1;
 
+        int faceFactor = Math.Sign(faceDirection) == Math.Sign(transform.localScale.x) ? 1 : -1;
         transform.localScale = new Vector3(
             transform.localScale.x * faceFactor,
             transform.localScale.y,
